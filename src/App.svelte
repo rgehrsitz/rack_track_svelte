888f00117226ad2e0372bb4d7b4a699822833913
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-07-09 20:20:33 -0400

    Sidebar kinda working

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,16 @@
     FooterLink,
     FooterLinkGroup,
   } from "flowbite-svelte";
+  import { onMount } from "svelte";
   import {
+    DarkMode,
+    Navbar,
+    NavBrand,
+    NavLi,
+    NavUl,
+    NavHamburger,
+    Drawer,
+    CloseButton,
     Sidebar,
     SidebarBrand,
     SidebarCta,
@@ -19,36 +28,103 @@
     SidebarItem,
     SidebarWrapper,
   } from "flowbite-svelte";
-  let spanClass = "flex-1 ml-3 whitespace-nowrap";
+  import { sineIn } from "svelte/easing";
+
+  let transitionParams = {
+    x: -320,
+    duration: 200,
+    easing: sineIn,
+  };
+
+  let breakPoint: number = 1024;
+  let width: number;
+  let backdrop: boolean = false;
+  let activateClickOutside = true;
+  let drawerHidden: boolean = false;
+  $: if (width >= breakPoint) {
+    drawerHidden = false;
+    activateClickOutside = false;
+  } else {
+    drawerHidden = true;
+    activateClickOutside = true;
+  }
+  onMount(() => {
+    if (width >= breakPoint) {
+      drawerHidden = false;
+      activateClickOutside = false;
+    } else {
+      drawerHidden = true;
+      activateClickOutside = true;
+    }
+  });
+  const toggleSide = () => {
+    if (width < breakPoint) {
+      drawerHidden = !drawerHidden;
+    }
+  };
+  const toggleDrawer = () => {
+    drawerHidden = false;
+  };
+
+  let spanClass =
+    "pl-2 self-center text-md text-gray-900 whitespace-nowrap dark:text-white";
+  let darkmodebtn =
+    "text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-lg p-2.5 fixed right-2 top-12  md:top-3 md:right-2 z-50";
+  let divClass = "w-full md:block md:w-auto pr-8";
+  let ulClass =
+    "flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-lg md:font-medium";
 </script>
 
-<main class="container">
-  <h1>Welcome to Tauri!</h1>
-  <Alert>
-    <span class="font-medium">Info alert!</span> Change a few things up and try submitting
-    again.
-  </Alert>
+<svelte:window bind:innerWidth={width} />
 
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
-    </a>
+<Navbar let:hidden let:toggle>
+  <NavHamburger on:click={toggleDrawer} btnClass="ml-3 lg:hidden" />
+  <NavBrand href="/" class="lg:ml-64">
+    <span
+      class="self-center whitespace-nowrap text-xl font-semibold dark:text-white pl-4"
+    >
+      My Website
+    </span>
+  </NavBrand>
+  <NavHamburger on:click={toggle} />
+  <NavUl {hidden} {divClass} {ulClass}>
+    <NavLi href="/">Home</NavLi>
+    <NavLi href="/pages/about">About</NavLi>
+    <NavLi
+      href="https://github.com/shinokada/flowbite-sveltekit-responsive-sidebar-layout"
+      >GitHub</NavLi
+    >
+  </NavUl>
+</Navbar>
+<DarkMode btnClass={darkmodebtn} />
+<Drawer
+  transitionType="fly"
+  {backdrop}
+  {transitionParams}
+  bind:hidden={drawerHidden}
+  bind:activateClickOutside
+  width="w-64"
+  class="overflow-scroll pb-32"
+  id="sidebar"
+>
+  <div class="flex items-center">
+    <CloseButton
+      on:click={() => (drawerHidden = true)}
+      class="mb-4 dark:text-white lg:hidden"
+    />
   </div>
-
-  <p>Click on the Tauri, Vite, and Svelte logos to learn more.</p>
-
-  <div class="row">
-    <Greet />
-  </div>
-  <Sidebar>
-    <SidebarWrapper>
+  <Sidebar asideClass="w-54">
+    <SidebarWrapper
+      divClass="overflow-y-auto py-4 px-3 rounded dark:bg-gray-800"
+    >
       <SidebarGroup>
+        <SidebarItem label="Home" href="/" on:click={toggleSide} />
+        <SidebarItem
+          label="About"
+          href={`/pages/`}
+          {spanClass}
+          on:click={toggleSide}
+        />
         <SidebarItem label="Dashboard">
           <svelte:fragment slot="icon">
             <svg
@@ -70,126 +146,16 @@
             >
           </svelte:fragment>
         </SidebarItem>
-        <SidebarItem label="Kanban" {spanClass}>
-          <svelte:fragment slot="icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-              ><path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
-              /></svg
-            >
-          </svelte:fragment>
-          <svelte:fragment slot="subtext">
-            <span
-              class="inline-flex justify-center items-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300"
-              >Pro</span
-            >
-          </svelte:fragment>
-        </SidebarItem>
-        <SidebarItem label="Inbox" {spanClass}>
-          <svelte:fragment slot="icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-              ><path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M9 3.75H6.912a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859M12 3v8.25m0 0l-3-3m3 3l3-3"
-              /></svg
-            >
-          </svelte:fragment>
-          <svelte:fragment slot="subtext">
-            <span
-              class="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-primary-600 bg-primary-200 rounded-full dark:bg-primary-900 dark:text-primary-200"
-              >3</span
-            >
-          </svelte:fragment>
-        </SidebarItem>
-        <SidebarItem label="Sidebar" href="/docs/components/sidebar">
-          <svelte:fragment slot="icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-              ><path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-              /></svg
-            >
-          </svelte:fragment>
-        </SidebarItem>
-        <SidebarItem label="Sign In">
-          <svelte:fragment slot="icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-              ><path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
-              /></svg
-            >
-          </svelte:fragment>
-        </SidebarItem>
-        <SidebarItem label="Sign Up">
-          <svelte:fragment slot="icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-              ><path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M4.5 12a7.5 7.5 0 0015 0m-15 0a7.5 7.5 0 1115 0m-15 0H3m16.5 0H21m-1.5 0H12m-8.457 3.077l1.41-.513m14.095-5.13l1.41-.513M5.106 17.785l1.15-.964m11.49-9.642l1.149-.964M7.501 19.795l.75-1.3m7.5-12.99l.75-1.3m-6.063 16.658l.26-1.477m2.605-14.772l.26-1.477m0 17.726l-.26-1.477M10.698 4.614l-.26-1.477M16.5 19.794l-.75-1.299M7.5 4.205L12 12m6.894 5.785l-1.149-.964M6.256 7.178l-1.15-.964m15.352 8.864l-1.41-.513M4.954 9.435l-1.41-.514M12.002 12l-3.75 6.495"
-              /></svg
-            >
-          </svelte:fragment>
-        </SidebarItem>
       </SidebarGroup>
     </SidebarWrapper>
   </Sidebar>
+</Drawer>
 
-  <Footer>
-    <FooterCopyright href="/" by="Flowbite™" year={2023} />
-    <FooterLinkGroup
-      ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
-    >
-      <FooterLink href="/">About</FooterLink>
-      <FooterLink href="/">Privacy Policy</FooterLink>
-      <FooterLink href="/">Licensing</FooterLink>
-      <FooterLink href="/">Contact</FooterLink>
-    </FooterLinkGroup>
-  </Footer>
-</main>
+<div class="flex px-4 mx-auto w-full">
+  <main class="lg:ml-72 w-full mx-auto">
+    <Greet />
+  </main>
+</div>
 
 <style>
-  .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #747bff);
-  }
-
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00);
-  }
 </style>
--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,20 +1,25 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/tauri";
   import { Button } from "flowbite-svelte";
-
-  let name = "";
-  let greetMsg = "";
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name });
-  }
 </script>
 
 <div>
-  <form class="row" on:submit|preventDefault={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <Button type="submit" color="blue">Greet</Button>
+  <h1>Welcome to Tauri!</h1>
+
+  <div class="row">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
+    </a>
+    <a href="https://tauri.app" target="_blank">
+      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank">
+      <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+
+  <p>Click on the Tauri, Vite, and Svelte logos to learn more.</p>
+  <form class="row">
+    <Button color="blue">Greet</Button>
   </form>
-  <p>{greetMsg}</p>
+  <p>Test</p>
 </div>
